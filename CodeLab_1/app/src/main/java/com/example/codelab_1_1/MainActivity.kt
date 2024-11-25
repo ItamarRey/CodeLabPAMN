b56208fd61401078a0c8aab32e424dfa844ca5ae
ItaLow32 <itamarreyrguez@gmail.com>
2024-11-25 23:31:30 +0000

    yesss

---
+++ CodeLab_1/app/src/main/java/com/example/codelab_1_1/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.codelab_1_1
+
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.codelab_1_1.ui.theme.PersonalCard
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PersonalCard {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PersonalText(
+                        message = "Itamar Rey",
+                        from = "Un desarrollador brillante",
+                        modifier = Modifier.padding(8.dp),
+                        contacto = "(+34) 691163469",
+                        share = "@ItaLow32",
+                        mail = "itamarreyrguez@gmail.com"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PersonalText(message: String, from: String, modifier: Modifier = Modifier, contacto: String, share: String, mail: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.itamar),
+            contentDescription = "Personal Image",
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = message,
+            fontSize = 40.sp,
+            color = Color(0xFF000000),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = from,
+            fontSize = 18.sp,
+            color = Color(0xFF006400),
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = contacto,
+            fontSize = 12.sp,
+            color = Color(0xFF000000),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = share,
+            fontSize = 12.sp,
+            color = Color(0xFF000000),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = mail,
+            fontSize = 12.sp,
+            color = Color(0xFF000000),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
---
+++ CodeLab_1/app/src/main/java/com/example/codelab_1_1/ui/theme/Theme.kt
@@ -0,0 +1,77 @@
+package com.example.codelab_1_1.ui.theme
+
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xFFD0F0D8),
+    secondary = PurpleGrey40,
+    tertiary = Pink40,
+
+    background = Color(0xFFD0F0D8),
+    surface = Color(0xFFD0F0D8),
+
+    onPrimary = Color.Black,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black
+)
+
+
+
+
+@Composable
+fun CodeLab_1_1Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
+@Composable
+fun PersonalCard(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
